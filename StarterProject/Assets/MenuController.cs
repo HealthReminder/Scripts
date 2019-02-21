9ed3f2596d903ae0913db01d414c23e63c5396e2
HealthReminder <eduardo.francio@hotmail.com>
2019-02-21 01:14:25 -0300

    StarterProject added
    It includes a menu manager and game manager

---
+++ StarterProject/Assets/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject mainMenuObject;
+    //Make it a singleton
+	[HideInInspector]    public static MenuController instance;
+    void Awake(){
+		//Singleton pattern
+		if  (instance == null){
+			DontDestroyOnLoad(gameObject);
+			instance = this;
+		}	
+		else if (instance != this)
+			Destroy(gameObject);
+		
+	}
+
+    //This function is responsible for turning the main menu on or off
+    public void ShowMainMenu (bool isActive) {
+        if(isActive){
+            mainMenuObject.SetActive(true);
+        } else {
+            mainMenuObject.SetActive(false);
+        }
+        
+    }
+
+}
